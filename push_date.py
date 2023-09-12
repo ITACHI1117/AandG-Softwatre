3d338da3c58b760a60ab895150a3fe6b07c95d61
unknown <ajogujoseph0317@gmail.com>
2023-09-12 16:16:20 +0100

    adding automatic push

--- push_date.py
+++ push_date.py
@@ -78,8 +78,8 @@ print(current_Month)
 
 
 
-Start_Date = [20,2,2023]
-End_Date = [25,2,2023]
+Start_Date = [25,1,2023]
+End_Date = [30,1,2023]
 print(Start_Date[1])
 print(current_Month)
 
@@ -137,11 +137,11 @@ def date_formater(S_date,E_date):
 
     if S_date[1] == "Apr" or S_date[1] == "Jun" or S_date[1] == "Sep" or S_date[1] == "Nov":
         E_date[0] = 30
-    elif S_date[1] == 2 or int(current_Year) %4 !=0 or E_date[0] > 28:
+    elif S_date[1] == "Feb" and int(current_Year) %4 !=0  and S_date[0] >= 25:
         E_date[0] = 28
-    elif S_date[1] == 2 or int(current_Year) == 0 or E_date[0] > 29:
+    elif S_date[1] == "Feb" and int(current_Year) == 0 and  S_date[0] >= 25:
         E_date[0] = 29
-    else:
+    elif E_date[0] >=30:
         E_date[0] = 31
 
 
